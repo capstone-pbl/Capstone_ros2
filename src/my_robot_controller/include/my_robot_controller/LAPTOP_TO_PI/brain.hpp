666c390f8bcf11784c4da56c24b9d5bb6681473d
seokjun <zoon0822@naver.com>
2026-04-29 21:53:36 +0900

    update brain node

--- src/my_robot_controller/include/my_robot_controller/LAPTOP_TO_PI/brain.hpp
+++ src/my_robot_controller/include/my_robot_controller/LAPTOP_TO_PI/brain.hpp
@@ -1,7 +1,11 @@
 #pragma once
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
+#include <map>
+#include <string>
+#include <algorithm>
 class Brain:public rclcpp::Node
 {
  public:
@@ -10,14 +14,16 @@ class Brain:public rclcpp::Node
 
 
  private:
-
+ std::map<std::string,std::pair<double,double>>zone_coords;
  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr calling_;
  rclcpp::TimerBase::SharedPtr timer_;
+ rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr nav_client_;
  enum State
  {
   IDLE , MOVING,ARRIVED,RETURNING
 
  };
  State current_state = IDLE;
- std::string zone_;
+ std::string zone_name;
+
 };
--- src/my_robot_controller/src/LAPTOP_TO_PI/brain.cpp
+++ src/my_robot_controller/src/LAPTOP_TO_PI/brain.cpp
@@ -2,75 +2,111 @@
 #include <string>
 
 
+
+
+
 Brain::Brain():Node("Brain")
 {
 
+nav_client_=rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this,"navigate_to_pose");
+zone_coords["A"]={1.0,2.0};
+ zone_coords["B"]={3.0,2.0};
+ zone_coords["home"]={0.0,0.0};
 calling_ = this->create_subscription<std_msgs::msg::String>("/call_zone",10,
 [this](std_msgs::msg::String::SharedPtr msg)
 {
  
- zone_ = msg -> data;
- RCLCPP_INFO(this->get_logger(),"호출: %s 구역", zone_.c_str());
- this->current_state=MOVING;
+ zone_name = msg -> data;
+ RCLCPP_INFO(this->get_logger(),"호출: %s 구역", zone_name.c_str());
+ if(zone_name=="home")this->current_state=RETURNING;
+ else this->current_state=MOVING;
+ double x,y;
+ auto goal = nav2_msgs::action::NavigateToPose::Goal();
+ goal.pose.header.frame_id="map";
+ goal.pose.header.stamp=this->now();
 
+
+
+  auto it=zone_coords.find(zone_name);
+  if(it != zone_coords.end())
+  {
+   x=it->second.first;   
+   y=it->second.second;
+
+   goal.pose.pose.position.x= x;
+   goal.pose.pose.position.y= y;
+
+    auto send_goal_options = rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions();
+    send_goal_options.result_callback = [this](auto result)
+    {
+    if(result.code == rclcpp_action::ResultCode::SUCCEEDED)
+    {
+       if(current_state==RETURNING) current_state=IDLE;
+       else current_state = ARRIVED;
+    }
+    };
+    nav_client_->async_send_goal(goal, send_goal_options);
+    if(current_state==MOVING)RCLCPP_INFO(this->get_logger(),"%s구역(%.1f , %.1f)으로 이동중",zone_name.c_str(),x,y); 
+    else if(current_state==RETURNING)RCLCPP_INFO(this->get_logger(),"HOME(%.1f , %.1f)으로 복귀중",x,y);
+  } 
 
 }
 );
 
-
 timer_ = this->create_wall_timer(std::chrono::milliseconds(100),
 [this]()
 {
-
+ static int count=0;
+ 
 switch(current_state)
-{
+{ 
  case IDLE:
  {  
-  static int count=0;
   if((count++ % 50)==0)RCLCPP_INFO(this->get_logger(),"[IDLE]명령 대기중");
 
  break;
  }
  
  case MOVING:
- 
  {
- if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[MOVING]%s구역으로 이동중",zone_.c_str());
+ if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[MOVING]%s구역으로 이동중",zone_name.c_str());
  
  break;
  
  }
+
  case ARRIVED:
  {
  
- if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[ARRIVED]%s구역에 도착. 사용자 탐색중",zone_.c_str());
+ if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[ARRIVED]%s구역에 도착. 사용자 탐색중",zone_name.c_str());
  
  break;
  
  }
+
  case RETURNING:
- 
  {
- if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[RETURNING]%s구역 탐색 마치고 복귀중",zone_.c_str());
+ if((count++ % 5)==0)RCLCPP_INFO(this->get_logger(),"[RETURNING]%s구역 탐색 마치고 복귀중",zone_name.c_str());
  
  break;
-
+ 
  }
-
 }
-
-
-
 }
-
-
-
-
 );
 
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
 
 int main(int argc,char ** argv)
 {
